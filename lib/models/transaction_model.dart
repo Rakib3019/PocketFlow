6aa3f5048eed8f47872e199c9552cded8c72c50e
Md. Rakib Islam <mdrakibislam1975@gmail.com>
2026-07-22 23:17:36 +0600

    updated transaction model

--- lib/models/transaction_model.dart
+++ lib/models/transaction_model.dart
@@ -2,46 +2,47 @@ class TransactionModel {
   final String id;
   final String title;
   final double amount;
-  final String category;
-  final String iconName;
-  final int colorValue;
   final bool isIncome;
+
+  final String categoryId;
   final DateTime date;
+
+  final String paymentMethod;
+
+  final String note;
 
   TransactionModel({
     required this.id,
     required this.title,
     required this.amount,
-    required this.category,
-    required this.iconName,
-    required this.colorValue,
     required this.isIncome,
+    required this.categoryId,
     required this.date,
+    required this.paymentMethod,
+    required this.note,
   });
 
-  Map<String, dynamic> toJson() {
-    return {
-      "id": id,
-      "title": title,
-      "amount": amount,
-      "category": category,
-      "iconName": iconName,
-      "colorValue": colorValue,
-      "isIncome": isIncome,
-      "date": date.toIso8601String(),
-    };
-  }
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "title": title,
+    "amount": amount,
+    "isIncome": isIncome,
+    "categoryId": categoryId,
+    "date": date.toIso8601String(),
+    "paymentMethod": paymentMethod,
+    "note": note,
+  };
 
   factory TransactionModel.fromJson(Map<String, dynamic> json) {
     return TransactionModel(
       id: json["id"],
       title: json["title"],
-      amount: json["amount"],
-      category: json["category"],
-      iconName: json["iconName"],
-      colorValue: json["colorValue"],
+      amount: (json["amount"] as num).toDouble(),
       isIncome: json["isIncome"],
+      categoryId: json["categoryId"],
       date: DateTime.parse(json["date"]),
+      paymentMethod: json["paymentMethod"],
+      note: json["note"] ?? "",
     );
   }
 }
